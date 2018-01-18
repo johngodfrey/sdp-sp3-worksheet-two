fab9872c6acc977d865e422b1db456e59f737b94
John Godfrey <jgodfr04@dcs.bbk.ac.uk>
2018-01-18 20:49:24 +0000

    Ex01 done.

--- exercise-one/src/Reflect.kt
+++ exercise-one/src/Reflect.kt
@@ -1,8 +1,40 @@
+import java.lang.reflect.Modifier
+
 fun main(args: Array<String>) {
     if (args.size != 1) {
         println("Requires a class name as argument to program")
         System.exit(-1)
     }
 
-    // TODO
+    val clazz = Class.forName(args[0])
+    val name = args[0]
+    val modifiers = clazz.modifiers
+    val isInterface = Modifier.isInterface(modifiers)
+    val interfaceOrClass = if (isInterface) "an interface" else "a class"
+    val mods = Modifier.toString(modifiers)
+    val constructors = StringBuilder()
+    for (cons in clazz.constructors) {
+        constructors.append("\t")
+        constructors.append(cons)
+        constructors.append("\n")
+    }
+    val methods = StringBuilder()
+    for (method in clazz.methods) {
+        methods.append("\t")
+        methods.append(method)
+        methods.append("\n")
+    }
+    val fields = StringBuilder()
+    for (field in clazz.fields) {
+        fields.append("\t")
+        fields.append(field)
+        fields.append("\n")
+    }
+    println("\nYou entered $name which is $interfaceOrClass, and its modifiers are $mods.")
+    val constructorString = if (clazz.constructors.isNotEmpty()) "\nIts constructors are:\n$constructors" else ""
+    print(constructorString)
+    val methodString = if (clazz.methods.isNotEmpty()) "\nIts methods are:\n$methods" else ""
+    print(methodString)
+    val fieldsString = if (clazz.fields.isNotEmpty()) "\nIts fields are:\n$fields" else ""
+    println(fieldsString)
 }
