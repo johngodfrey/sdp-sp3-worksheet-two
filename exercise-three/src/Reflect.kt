2877ae77d5c21b648adc830e3ccbf070cf10f180
John Godfrey <jgodfr04@dcs.bbk.ac.uk>
2018-01-19 15:47:12 +0000

    Ex03 correctly tests for non-private fields.

--- exercise-three/src/Reflect.kt
+++ exercise-three/src/Reflect.kt
@@ -1,4 +1,3 @@
-class HypotheticalAssignment(val field1: Any?, val field2: Any?, val field3: Any?, val field4: Any?) {
-
-
+class HypotheticalAssignment(public var field1: Any?, private val field2: Any?, val field3: Any?, val field4: Any?) {
+    // @JvmField val field5: Any? = "w" // causes field count and field privacy tests to fail
 }
--- exercise-three/test/HypotheticalAssignmentTest.kt
+++ exercise-three/test/HypotheticalAssignmentTest.kt
@@ -1,13 +1,26 @@
 import org.junit.Assert.*
 import org.junit.Test
+import java.lang.reflect.Modifier
 
 class HypotheticalAssignmentTest {
     private var assignment = HypotheticalAssignment(null, null, null, null)
+    private val clazz = assignment.javaClass
 
     @Test
     fun testNumberOfFields() {
-        val clazz = assignment.javaClass
         assertTrue("Assignment has ${clazz.declaredFields.size} fields.", clazz.declaredFields.size < 5)
     }
 
+    @Test
+    fun testForNonPrivateFields() {
+        var allFieldsPrivate = true
+        for (field in clazz.declaredFields) {
+            println(Modifier.isPrivate(field.modifiers))
+            println(field)
+            if (!Modifier.isPrivate(field.modifiers)) {
+                allFieldsPrivate = false
+            }
+        }
+        assertTrue(allFieldsPrivate)
+    }
 }
